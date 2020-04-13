c7523dc1658fc291b340b126761f2ced990b26ec
dav190692 <dav190692@gmail.com>
2020-04-13 13:12:41 +0400

    ImpExp

--- src/app.js
+++ src/app.js
@@ -1,3 +1,65 @@
-export  const  func = () => {
-    alert("test");
-};
+
+function addForm(){
+    let form = document.createElement('form');
+    form.id = "formID";
+    document.body.append(form);
+    let addLabel = document.createElement('label');
+    addLabel.setAttribute("for", "addText");
+    addLabel.innerHTML = "Write Text";
+    form.append(addLabel);
+    let addInput = document.createElement('input');
+    addInput.type = "text";
+    addInput.id = "addText";
+    form.append(addInput);
+    let addClick = document.createElement('input');
+    addClick.type = "button";
+    addClick.id = "addButton";
+    addClick.value = "ADD";
+    form.append(addClick);
+
+}
+
+// addForm();
+
+
+
+function add() {
+    let i = 0;
+    let div = document.createElement('div');
+    if (addText.value) {
+        i++
+        div.innerHTML = addText.value;
+        div.className = `valueDiv-${i}`;
+        div.id = `valueDiv-${i}`;
+        let del = document.createElement('input');
+        del.setAttribute('type', 'button');
+        del.className = `Delete-${i}`;
+        del.value = "Delete";
+        // console.log(form);
+        document.body.append(div);
+        // form.after(div);
+        div.append(del);
+        // del.onclick = del1();
+        del.addEventListener("click", del1);
+        // div.append(del);
+        // form.after(div);
+        
+    }
+    
+}
+   
+function del1(){ 
+console.log(this.className);
+const index = this.className.split("-")[1];
+console.log(index);
+document.getElementById("valueDiv-" + index).remove();
+}
+
+
+
+
+
+
+// addButton.addEventListener("click", add);
+
+export{addForm, add, del1};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+/* let root = document.getElementById('root');
+root.innerHTML = "TEXT"; */
 
 
 
-
-let root = document.getElementById('root');
-root.innerHTML = "TEXT";
+import {addForm, add, del1} from './app';
+addForm();
+addButton.addEventListener("click", add);
